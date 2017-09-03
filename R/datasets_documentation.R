d69103ba2335a495579f475d8799ec3ad319431b
Gert Janssenswillen <gert.janssenswillen@uhasselt.be>
2017-09-03 20:39:06 +0000

    version 0.1.1

--- R/datasets_documentation.R
+++ R/datasets_documentation.R
@@ -55,3 +55,13 @@
 #' @format Eventlog containing 500 patient cases
 #' @keywords datasets
 "patients"
+
+#' @name sepsis
+#' @title Sepsis Cases - Event Log
+#' @description This real-life event log contains events of sepsis cases from a hospital. Sepsis is a life threatening condition typically caused by an infection. One case represents the pathway through the hospital. The events were recorded by the ERP (Enterprise Resource Planning) system of the hospital. There are about 1000 cases with in total 15,000 events that were recorded for 16 different activities. Moreover, 39 data attributes are recorded, e.g., the group responsible for the activity, the results of tests and information from checklists. Events and attribute values have been anonymized. The time stamps of events have been randomized, but the time between events within a trace has not been altered.
+#'#' @docType data
+#' @formate Eventlog containing 1050 cases
+#' @keywords datasets
+#' @references Analyzing the Trajectories of Patients with Sepsis using Process Mining (Felix Mannhardt and Daan Blinde, CEUR Workshop Proceedings Vol-1859 RADAR+EMISA 2017)
+#' @source <http://data.4tu.nl/repository/uuid:915d2bfb-7e84-49ad-a286-dc35f063a460>
+"sepsis"
